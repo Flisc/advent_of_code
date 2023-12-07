375bbdd94ea53c3ba48d5dddd42d443792e0769e
Flisc <fliscadrian@yahoo.com>
2023-12-07 23:06:03 +0200

    day 1 && 2

--- src/Utils.kt
+++ src/Utils.kt
@@ -6,7 +6,7 @@ import kotlin.io.path.readLines
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readLines()
+fun readInput(name: String, day: String) = Path("src/$day/$name.txt").readLines()
 
 /**
  * Converts string to md5 hash.
@@ -18,4 +18,4 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
 /**
  * The cleaner shorthand for printing output.
  */
-fun Any?.println() = println(this)
+fun Any?.print() = println(this)
---
+++ src/day_2/Game.kt
@@ -0,0 +1,9 @@
+package day_2
+
+data class Game(
+    val id: Int,
+    val rounds: MutableList<Round>
+)
+data class Round(
+    val numberByColor: Map<String, Int>
+)
