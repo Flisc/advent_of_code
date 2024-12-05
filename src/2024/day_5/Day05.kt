b2eee163264c18707b724a86b49832543d51b218
AFL <adrian.flisc@fraba.com>
2024-12-05 14:49:50 +0200

    day05

--- src/2024/day_5/Day05.kt
+++ src/2024/day_5/Day05.kt
@@ -8,8 +8,8 @@ fun main() {
     var matrix = mutableListOf<List<String>>()
     var sum = 0
     var pairRules =  mutableListOf<Pair<Int, Int>>()
-    val updates = mutableListOf<List<Int>>()
-    val validUpdates = mutableListOf<List<Int>>()
+    var updates = mutableListOf<List<Int>>()
+    var validUpdates = mutableListOf<List<Int>>()
 
     fun mapInput(input: List<String>) {
         input.forEach { line ->
@@ -29,40 +29,95 @@ fun main() {
     fun List<Pair<Int, Int>>.containsSecond(second: Int) =
         this.filter { it.second == second }.size != 0
 
+    fun List<Pair<Int, Int>>.matchSecond(second: Int): List<Pair<Int, Int>> =
+        this.filter { it.second == second }
+
+    fun resetData() {
+        sum = 0
+        pairRules = mutableListOf<Pair<Int, Int>>()
+        updates = mutableListOf<List<Int>>()
+        validUpdates = mutableListOf<List<Int>>()
+    }
+
     fun part1(input: List<String>): Int {
+        resetData()
         mapInput(input)
+
+
         for(pages in updates) {
+            var validPage = true
             for(i in 0..pages.size-1) {
                 val currentRules = pairRules.matchFirst(pages[i])
-                var validPage = false
-                pages.subList(i+1, pages.size).forEach {
-                    if (currentRules.containsSecond(it)) {
-                        validPage = true
+                currentRules.forEach {
+                    if(pages.contains(it.second) &&
+                        !pages.subList(i+1, pages.size).contains(it.second)) {
+                        validPage = false
                     }
                 }
-                println()
             }
+            if(validPage) {
+                validUpdates.add(pages)
+            }
+            validPage = false
         }
 
-        return sum
+        return validUpdates.map { it[it.size/2] }.sum()
     }
 
     fun part2(input: List<String>): Int {
+        resetData()
+        mapInput(input)
+        var invalidUpdates = mutableListOf<List<Int>>()
 
-        return sum
+        for(pages in updates) {
+            var validPage = true
+            var wrongPoints = mutableMapOf<Int, Int>()
+            for(i in 0..pages.size-1) {
+                val currentRules = pairRules.matchFirst(pages[i])
+                currentRules.forEach {
+                    if(pages.contains(it.second) &&
+                        !pages.subList(i+1, pages.size).contains(it.second)) {
+                        validPage = false
+                    }
+                }
+            }
+
+            if(validPage.not()) {
+                var newPages = mutableListOf<Int>()
+                newPages.addAll(pages)
+                for(i in 0..newPages.size-1) {
+                    val currentRule = pairRules.matchFirst(newPages[i])
+                    val badPoints = newPages.subList(0, i+1).intersect(currentRule.map { it.second })
+                    if(badPoints.isNotEmpty()) {
+                        badPoints.forEach {
+                            var temp = newPages[i]
+                            newPages[i] = newPages[newPages.indexOf(it)]
+                            newPages[newPages.indexOf(it)] = temp
+                        }
+                    }
+                }
+                invalidUpdates.add(newPages)
+            }
+            validPage = true
+        }
+
+        invalidUpdates.prettyPrint()
+
+
+        return invalidUpdates.map { it[it.size/2] }.sum()
     }
 
-    val testInput = readInput("example", "day_5")
-    part1(testInput).prettyPrint("example")
-
-//    val part1 = readInput("puzzle", "day_4")
+//    val testInput = readInput("example", "day_5")
+//    part1(testInput).prettyPrint("example")
+//
+//    val part1 = readInput("puzzle", "day_5")
 //    part1(part1).prettyPrint("part1")
 //
-//    val part2 = readInput("example", "day_4")
-//    part2(part2).prettyPrint("part2")
+    val part2 = readInput("example", "day_5")
+    part2(part2).prettyPrint("part2")
 //
-//    val part2p = readInput("puzzle", "day_4")
-//    part2(part2p).prettyPrint("part2")
+    val part2p = readInput("puzzle", "day_5")
+    part2(part2p).prettyPrint("part2")
 
 }
 
