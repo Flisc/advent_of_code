a5f746479843f417c4ff858493ea1ac8b52b0850
AFL <adrian.flisc@fraba.com>
2024-12-11 09:36:26 +0200

    day11 wip

---
+++ src/2024/day_11/Day11.kt
@@ -0,0 +1,84 @@
+package `2024`.day_11
+
+import Util.Companion.prettyPrint
+import printMatrix
+import readInput
+
+
+fun main() {
+    val dayNumber = "day_11"
+    var res = 0L
+    var stones = mutableListOf<Long>()
+
+    fun mapInput(input: List<String>) {
+            input.forEachIndexed { index, line ->
+                val row = line.split(" ")
+                    .filter { it.isNotEmpty() }
+                    .map { it.toString().toLongOrNull() ?: -1 }
+                    .toMutableList()
+                stones.addAll(row)
+            }
+        println()
+    }
+
+    fun resetData() {
+        res = 0L
+        stones = mutableListOf()
+    }
+
+    fun Long.applyRule(): List<Long> {
+        return when {
+            this == 0L -> listOf(1L)
+            this.toString().length % 2 == 0 -> {
+                val digits = this.toString()
+                val mid = digits.length / 2
+                val left = digits.substring(0, mid).toLong()
+                val right = digits.substring(mid).toLong()
+                listOf(left, right)
+            }
+            else -> listOf(this * 2024)
+        }
+    }
+
+    fun Set<Pair<Pair<Int, Int>, Pair<Int, Int>>>.printRoutePoints() {
+        this.forEachIndexed { index, pairs ->
+            val sb = StringBuilder()
+            sb.append(" [${pairs.first}, ${pairs.second}] ")
+            println(sb.toString())
+            sb.clear()
+        }
+    }
+
+
+
+    fun part1(input: List<String>): Long {
+        resetData()
+        mapInput(input)
+
+        return res
+    }
+
+    fun part2(input: List<String>): Long {
+        resetData()
+        mapInput(input)
+
+
+        return res
+    }
+
+    val testInput = readInput("example", dayNumber)
+    part1(testInput).prettyPrint("example")
+
+//    val part1 = readInput("puzzle", dayNumber)
+//    part1(part1).prettyPrint("part1")
+
+
+//    val part2 = readInput("example", "day_9")
+//    part2(part2).prettyPrint("part2")
+
+
+//    val part2p = readInput("puzzle", "day_9")
+//    part2(part2p).prettyPrint("part2")
+
+}
+
