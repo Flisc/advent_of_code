825c9ef2fc10e8538aeba68327c0b4722cba4f1f
AFL <adrian.flisc@fraba.com>
2024-12-12 12:08:29 +0200

    day12 wip

---
+++ day_12/Models.py
@@ -0,0 +1,6 @@
+class Direction:
+    def __init__(self, direction):
+        self.direction = direction
+
+    def turn(self, direction):
+        self.direction = direction
--- day_12/day.py
+++ day_12/day.py
@@ -1,19 +1,35 @@
 from collections import defaultdict
 import pprint
 
+from day_12.Models import Direction
+
 PRINT_ENABLED = True
 file_name = 'example.txt'
 # file_name = 'input.txt'
 
 data = []
 plants_dict = defaultdict(list)
+perimeter_dict = defaultdict(int)
+
+def in_bounds(x, y):
+    return (x >= 0 and x < len(data) and
+            y >= 0 and y < len(data))
+
+def next_in_plot(plant_val, i, j, direction: Direction):
+    if PRINT_ENABLED:
+        print(f'current plot: {plant_val} dir: {direction}')
+    if not in_bounds(i+1, j+1):
+        return
+
+
 
 
 def search_plots():
     # curr_plant = data[0][0]
     for i in range(len(data)):
         for j in range(len(data)):
-            plants_dict[data[i][j]].append((i, j))
+            plants_dict[data[i][j]].append((i, j, False))
+
             # if curr_plant == data[i][j]:
             #     plants_dict[curr_plant].append((i, j))
             # else:
