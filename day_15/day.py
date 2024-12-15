b57f53712feec203b674e451154a4b387c0a7851
flisc <fliscadrian@yahoo.com>
2024-12-15 16:23:58 +0200

    day 15 wip

--- day_15/day.py
+++ day_15/day.py
@@ -8,7 +8,7 @@ file_name = 'example.txt'
 data = []
 moves = []
 res = 0
-
+current_point = [0, 0]
 
 with open(file_name) as f:
     for line in f.read().strip().split('\n'):
@@ -19,39 +19,84 @@ with open(file_name) as f:
             moves = item
 
 
-def go(i, j, direction):
-    if(direction == "^"):
-        print(f"\n go up from {i}, {j} ")
-        if(data[i-1][j] == "#"):
+def go(row, col, direction):
+    if direction == "^": #done
+        if data[row-1][col] == "#":  # wall
             return
-        else:
-            #
-            print(f"\n shift up col from pos {i}, {j}")
-            for x in reversed(range(i)):
-                if data[x][j] == "O":
-                    if data[x-1][j] == "#":
-                        return
-                    else:
-                        data[x-1][j] = "O"
-                        data[x][j] = "@"
-                elif data[x][j] == ".":
-                    data[x][j] = "@"
-                    data[x+1][j] = "."
 
+        box_segment_start = row
+        box_segment_end = row
+        if data[row - 1][col] == "O":
+            while (box_segment_end - 1 > 0
+                   and data[box_segment_end-1][col] == "O"):
+                box_segment_end -= 1
+            data[box_segment_end - 1][col] = "O"
+            data[box_segment_start][col] = "@"
+            data[current_point[0]][current_point[1]] = "."
+        elif data[row - 1][col] == ".":
+             data[row - 1][col] = "@"
+             data[row][col] = "."
+             current_point[0] -= 1
 
+    elif direction == "V":
+        for x in range(row, len(data) - 1):
+            if data[x][col] == "O":
+                if data[x + 1][col] == "#":
+                    return
+                else:
+                    data[x + 1][col] = "O"
+                    data[x][col] = "@"
+            elif data[x][col] == ".":
+                data[x][col] = "@"
+                data[x - 1][col] = "."
+
+    elif direction == "<":
+        for y in reversed(range(col)):
+            if data[row][y] == "O":
+                if data[row][y - 1] == "#":
+                    return
+                else:
+                    data[row][y - 1] = "O"
+                    data[row][y] = "@"
+            elif data[row][y] == ".":
+                data[row][y] = "@"
+                data[row][y + 1] = "."
+
+    elif direction == ">": # done
+        if data[row][col + 1] == "#": # wall
+            return
+
+        if data[row][col+1] == "O":
+            box_segment_start = col+1
+            box_segment_end = col+1
+            while box_segment_end + 1 < len(data)-1 and data[row][box_segment_end + 1] == "O":
+                box_segment_end += 1
+            # TODO: check for wall
+            data[row][box_segment_end+1] = "O"
+            data[row][box_segment_start] = "@"
+            data[current_point[0]][current_point[1]] = "."
+            current_point[1] += 1
+            return
+        elif data[row][col+1] == ".":
+             data[current_point[0]][current_point[1]] = "."
+             current_point[1] += 1
+             data[current_point[0]][current_point[1]] = "@"
 
 
 for i in range(len(data)):
     for j in range(len(data)):
-        if data[i][j] == "@":
-            # for move in moves:
-                go(i, j, "^")
+        if data[i][j] == '@':
+            print(f"found at {i}, {j}")
+            current_point = [i, j]
 
-
+for move in moves:
+    print(f"GO {move}")
+    go(current_point[0],current_point[1], move)
+    pprint.pprint(data)
 
 if PRINT_ENABLED:
     print("\n\n")
-    pprint.pprint(data)
+    # pprint.pprint(data)
     print("moves")
     pprint.pprint(moves)
 
