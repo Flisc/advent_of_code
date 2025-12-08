479fbb2189e601e266a94dd8074ace1cd9e1b1ff
Flisc <fliscadrian@yahoo.com>
2025-12-08 13:07:27 +0200

    [2025][day_8][part_2]-example

--- src/2025/day_8/Day.kt
+++ src/2025/day_8/Day.kt
@@ -110,9 +110,88 @@ class Day {
     }
 
     fun part2(lines: List<String>): Long {
-        var sum: Long = 0
+        var res: Long = 0
+        // map input
+        lines.forEach { line ->
+            val coordinates = line.split(",").map { it.toInt() }
+            boxes.add(Box(Point(coordinates[0], coordinates[1], coordinates[2])))
+        }
+        val distances: MutableList<Circuit> = mutableListOf()
 
-        return sum
+        // combinations of n taken by k
+        boxes.forEachIndexed { index, box ->
+            for (i in index+1 until boxes.size) {
+                distances.add(Circuit(box, boxes[i], box.distanceTo(boxes[i])))
+            }
+        }
+        distances.sortBy { it.distance }
+
+        val circuits: MutableList<MutableSet<String>> = mutableListOf()
+        val mappedIds = mutableSetOf<String>()
+        var i:Int = 0
+        for (c in distances) {
+//            if (circuits.size == 1) break
+//            if (i >= 1000) break
+            if (circuits.isEmpty()) {
+                circuits.add(mutableSetOf(c.from.id, c.to.id))
+                mappedIds.add(c.from.id)
+                mappedIds.add(c.to.id)
+                println("first circuit: $c")
+                i++
+                continue
+            }
+            val circuitWithFrom = circuits.firstOrNull { it.contains(c.from.id) }
+            val circuitWithTo = circuits.firstOrNull { it.contains(c.to.id) }
+
+            when {
+                circuitWithFrom != null && circuitWithFrom === circuitWithTo -> {
+                    println("already mapped circuit: ${c.from} - ${c.to}")
+                    i++
+                }
+                circuitWithFrom != null && circuitWithTo != null && circuitWithFrom !== circuitWithTo -> {
+                    println("MERGING circuits: ${circuits.indexOf(circuitWithFrom)} and ${circuits.indexOf(circuitWithTo)}")
+                    circuitWithFrom.addAll(circuitWithTo)
+                    circuits.remove(circuitWithTo)
+                    println("Merged circuit now has ${circuitWithFrom.size} boxes")
+                    i++
+                    println("Circuits size: ${circuits.size}")
+                    if (circuits.size == 1 && circuits[0].size == boxes.size) {
+                        println("Circuits merged by: ${c.from} - ${c.to}")
+                        break
+                    }
+                }
+                circuitWithFrom != null -> {
+                    circuitWithFrom.add(c.to.id)
+                    mappedIds.add(c.to.id)
+                    println("add to ${c.to} to circuit: ${circuits.indexOf(circuitWithFrom)}, size: ${circuitWithFrom.size}")
+                    i++
+                }
+                circuitWithTo != null -> {
+                    circuitWithTo.add(c.from.id)
+                    mappedIds.add(c.from.id)
+                    println("add from ${c.from} to circuit: ${circuits.indexOf(circuitWithTo)}, size: ${circuitWithTo.size}")
+                    i++
+                }
+                else -> {
+                    circuits.add(mutableSetOf(c.from.id, c.to.id))
+                    mappedIds.add(c.from.id)
+                    mappedIds.add(c.to.id)
+                    i++
+                    println("new circuit: $c")
+                }
+            }
+            println()
+            if (mappedIds.size == boxes.size && circuits.size == 1) {
+                println("1 Circuit by: $c")
+                return (c.from.position.x * c.to.position.x).toLong()
+            }
+
+        }
+
+        res = circuits.map { it.size }.sortedDescending().take(3).reduce { acc, i -> acc * i }.toLong()
+
+
+        return res
     }
 
 }
@@ -123,8 +202,8 @@ fun main() {
     measureTimeMillis {
 //        val example = readInput("example", runner.dayNumber, "2025")
         val example = readInput("puzzle", runner.dayNumber, "2025")
-        runner.part1(example).prettyPrint("res")
-//        runner.part2(example).prettyPrint("res")
+//        runner.part1(example).prettyPrint("res")
+        runner.part2(example).prettyPrint("res")
     }.also { println("Execution time: ${it}ms") }
 
 }
